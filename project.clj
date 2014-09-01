aab04a01f5d17fe910542b447fc067aeee912104
Dirk Geurs <dirklectisch@mac.com>
2014-09-02 00:25:05 +0200

    Updated deps to latest stable versions

--- project.clj
+++ project.clj
@@ -2,33 +2,30 @@
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
 
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2173"]
-                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [om "0.5.0"]
-                 [compojure "1.1.8"]
-                 [ring/ring-jetty-adapter "1.2.2"]
-                 [environ "0.5.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2322"]
+                 [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
+								 [om "0.7.1"]
+								 [compojure "1.1.8"]
+                 [ring/ring-jetty-adapter "1.3.1"]
+								 [environ "1.0.0"]]
 
   :min-lein-version "2.0.0"
 
-  :plugins [[lein-cljsbuild "1.0.2"]
-            [environ/environ.lein "0.2.1"]]
+  :plugins [[lein-cljsbuild "1.0.3"]
+            [lein-environ "1.0.0"]]
 
-  :hooks [environ.leiningen.hooks
-          leiningen.cljsbuild]
+  :hooks [leiningen.cljsbuild]
 
   :source-paths ["src"]
 
   :cljsbuild {
-    :builds {:dev {:id "chronologie"
-                   :source-paths ["src"]
+    :builds {:dev {:source-paths ["src"]
                    :compiler {
-                     :output-to "resources/public/chronologie.js"
-                     :output-dir "out/dev"
-                     :optimizations :whitespace }}
-             :prod {:id "chronologie"
-                    :source-paths ["src"]
+                     	:output-to "resources/public/chronologie.js"
+                     	:output-dir "out/dev"
+										 	:print-input-delimiter true }}
+             :prod {:source-paths ["src"]
                     :compiler {
                       :output-to "resources/public/chronologie.js"
                       :output-dir "out/prod"
@@ -36,4 +33,5 @@
 
   :uberjar-name "chronologie.jar"
   :main chronolgie.web
-  :profiles {:production {:env {:production true}}})
+  :profiles {:production {:env {:production true}}
+						 :uberjar {:aot :all}})
